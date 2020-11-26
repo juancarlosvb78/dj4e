a40ea0fbe4e859a6a2c9d5983c065c09c7ec7c4a
Charles Severance <csev@umich.edu>
2020-11-26 10:01:57 -0500

    Some initial text.

--- index.php
+++ index.php
@@ -11,11 +11,19 @@ require "nav.php";
 <div style="margin-left: 10px; float:right">
 <img src="images/Chuck_16x9_SakaiCar_DJ4E_small.png" onclick='window.location.href="https://www.sakaiger.com/sakaicar";' target="_blank" style="padding: 5px; width:360px;">
 </div>
-<h1>Django for Everybody</h1>
+<h1>Django para Todos</h1>
 <p>
-This web site is building a set of free materials, lectures, and assignments to help students
-learn the Django web development framework.  
-You can take this course and receive a certificate at:
+Este sitio esta en construcción. Si desea ayudar con las traducciones, consulte el repositorio de GitHub 
+<a href="https://github.com/csev-es/dj4e" target="_blank">
+https://github.com/csev-es/dj4e</a>.
+</p>
+<p>This is an <b>under construction</b> copy of 
+<a href="https://www.dj4e.com/" target="_blank">www.dj4e.com</a>.
+</p>
+<p>
+Este sitio web está creando un conjunto de materiales, conferencias y tareas gratuitos para ayudar a los estudiantes
+aprenda el marco de desarrollo web de Django.
+Puede realizar este curso y recibir un certificado en:
 <ul>
 <li><a href="https://www.coursera.org/specializations/django">Coursera: Django for Everybody Specialization</a></li>
 <li><a href="https://www.edx.org/xseries/michiganx-django-for-everybody">edX: Django for Everybody XSeries Program</a></li>
@@ -23,19 +31,22 @@ You can take this course and receive a certificate at:
 </ul>
 </p>
 <p>
-We use the free 
-<a href="https://www.pythonanywhere.com" target="_blank">PythonAnywhere</a> hosting environment
-to deploy and test our Django projects and applications.  You can keep using this hosting environent
-to develop and deploy your Django applications after you complete the course.
+Usamos el entorno de alojamiento gratuito 
+<a href="https://www.pythonanywhere.com" target="_blank">PythonAnywhere</a>
+para implementar y probar nuestros proyectos y aplicaciones de Django.
+Puede seguir utilizando este entorno de alojamiento para desarrollar e implementar 
+sus aplicaciones Django después de completar el curso.
 </p>
-<h2>Technology</h2>
+<h2>Tecnología</h2>
 <p>
-This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a> 
-framework to embed a learning 
-management system into this site and handle the autograders.  
-If you are interested in collaborating
-to build these kinds of sites for yourself, please see the 
-<a href="http://www.tsugi.org" target="_blank">tsugi.org</a> website.
+Este sitio utiliza
+<a href="http://www.tsugi.org" target="_blank"> Tsugi </a>
+marco para incorporar un aprendizaje
+sistema de gestión en este sitio y manejar los autograders.
+Si te interesa colaborar
+para crear este tipo de sitios para usted, consulte la
+<a href="http://www.tsugi.org" target="_blank"> tsugi.org </a>
+sitio web.
 <h3>Copyright</h3>
 <p>
 The material produced specifically for this site is by Charles Severance and others
--- nav.php
+++ nav.php
@@ -7,36 +7,36 @@ $set = new \Tsugi\UI\MenuSet();
 $set->setHome($CFG->servicename, $CFG->apphome);
 
 if ( isset($CFG->lessons) ) {
-    $set->addLeft('Lessons', $R.'lessons');
+    $set->addLeft('Lecciones', $R.'lessons');
 }
 if ( isset($_SESSION['id']) ) {
-    $set->addLeft('My Progress', $R.'assignments');
+    $set->addLeft('Asignaciones', $R.'assignments');
 } else {
-    $set->addLeft('Assignments', $R.'assn');
+    $set->addLeft('Asignaciones', $R.'assn');
 }
 
 if ( isset($_SESSION['id']) ) {
     $submenu = new \Tsugi\UI\Menu();
-    $submenu->addLink('Profile', $R.'profile');
+    $submenu->addLink('Perfil', $R.'profile');
     if ( isset($CFG->google_map_api_key) ) {
-        $submenu->addLink('Map', $R.'map');
+        $submenu->addLink('Mapa', $R.'map');
     }
     if ( isset($CFG->badge_path)  ) {
-        $submenu->addLink('Badges', $R.'badges');
+        $submenu->addLink('Insignias', $R.'badges');
     }
     if ( file_exists('materials.php') ) {
-        $submenu->addLink('Materials', $R.'materials');
+        $submenu->addLink('Materiales', $R.'materials');
     }
     if ( file_exists('privacy.php') ) {
-        $submenu->addLink('Privacy', $R.'privacy');
+        $submenu->addLink('Intimidad', $R.'privacy');
     }
     if ( $CFG->providekeys ) {
-        $submenu->addLink('LMS Integration', $T . 'settings');
+        $submenu->addLink('LMS Integración', $T . 'settings');
     }
     if ( isset($CFG->google_classroom_secret) ) {
         $submenu->addLink('Google Classroom', $T.'gclass/login');
     }
-    $submenu->addLink('Free App Store', 'https://www.tsugicloud.org');
+    $submenu->addLink('App Store gratis', 'https://www.tsugicloud.org');
     if ( $CFG->DEVELOPER ) {
         $submenu->addLink('Test LTI Tools', $T . 'dev');
     }
@@ -44,7 +44,7 @@ if ( isset($_SESSION['id']) ) {
     if ( isset($_COOKIE['adminmenu']) && $_COOKIE['adminmenu'] == "true" ) {
         $submenu->addLink('Administer', $T . 'admin/');
     }
-    $submenu->addLink('Logout', $R.'logout');
+    $submenu->addLink('Cerrar sesión', $R.'logout');
     if ( isset($_SESSION['avatar']) ) {
         $set->addRight('<img src="'.$_SESSION['avatar'].'" style="height: 2em;"/>', $submenu);
         // htmlentities($_SESSION['displayname']), $submenu);
@@ -52,7 +52,7 @@ if ( isset($_SESSION['id']) ) {
         $set->addRight(htmlentities($_SESSION['displayname']), $submenu);
     }
 } else {
-    $set->addRight('Login', $T.'login.php');
+    $set->addRight('Iniciar sesión', $T.'login.php');
 }
 $set->addRight('Instructor', 'http://www.dr-chuck.com');
 
